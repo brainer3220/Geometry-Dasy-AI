d79d133e7aae86b3bbec220ddfd13b81d66fb664
brainer3220 <40418544+brainer3220@users.noreply.github.com>
2019-03-24 12:16:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,28 @@ import numpy as np
 from PIL import image   #이미지 처리 라이브러리
 from tensorflow.keras import layers
 
+Play_Button = [361, 228]    #첫 시작 버튼
+next_Button = [980, 245]    # 게임내 목록 다음 버튼
+Previous = [42, 245]        #게임내 모곡 이전 버튼
+
 result = 0  #결과 반환 값, 추후 무슨 일을 할것인지 반환
 
 while True:
     x, y = pag.position()
     position_str = 'X: ' + str(x) + 'Y: '+ str(y)
     print(position_str)
+
+    def compute_icon_type(img):
+        mean = nm.mean(img, axis=(0, 1))
+
+        if mean[0] > 50 and mean[0] < 55 and mean[1] > 50 and mean[1] < 55 and mean[2] > 50 and mean[2] < 55:
+            result = "Play_Button"
+
+        else if mean[0] > 250 and mean[1] > 85 and mean[1] < 110 and mean[2] > 250:
+            result = "Lock"
+            
+with mss.mss() as sct:
+
 
 model = tf.keras.Sequential()
 # Adds a densely-connected layer with 64 units to the model:
@@ -21,14 +37,6 @@ model.add(layers.Dense(64, activation='relu'))
 # Add a softmax layer with 10 with 10 output units:
 model.add(layers.Dense(10, activation='softmax'))
 
-def compute_icon_type(img):
-    mean = nm.mean(img, axis=(0, 1))
-
-    if mean[0] > 50 and mean[0] < 55 and mean[1] > 50 and mean[1] < 55 and mean[2] > 50 and mean[2] < 55:
-        result = "Play_Button"
-
-    else if mean[0] > 250 and mean[1] > 85 and mean[1] < 110 and mean[2] > 250:
-        result = "Lock"
 
 Play_Button = compute_icon_type(Play_button)
 
