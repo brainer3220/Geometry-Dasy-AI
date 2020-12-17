ec114fa848eccb0fa35c35ed8e880f2b1aba12e1
brainer <brainer12345@naver.com>
2020-12-18 01:25:55 +0900

    Model update

--- Code/main.py
+++ Code/main.py
@@ -236,28 +236,28 @@ def GamePlay():
             else:
                 print("Miss")
 
-def BinaryImageClassf():
+
+def ImageClassf():
     model = Sequential()
     model.add(Conv2D(120, 60, 3, padding='same', activation='relu',
                         input_shape=(640, 360, 3)))
     model.add(MaxPooling2D(pool_size=(65, 25)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
     
     model.add(Conv2D(60, 30, 3, padding='same'))
     model.add(MaxPooling2D(pool_size=(60, 25), padding='same'))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
     
     model.add(Conv2D(60, 25, 3, padding='same'))
     model.add(MaxPooling2D(pool_size=(60, 25), padding='same'))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
     
     model.add(Flatten())
     model.add(Dense(256, activation = 'relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(128, activation = 'sigmoid'))
 
-    model.add(Dense(1, activation='softmax'))
-    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    model.add(Dense(2, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='Nadam', metrics=['accuracy'])
     return model
 
 
