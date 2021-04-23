27c861fd1bd321557f0fd6cab578c5d67a3f3c44
Restyled.io <commits@restyled.io>
2021-04-23 05:11:53 +0000

    Restyled by autopep8

--- Code/ImageClassf.py
+++ Code/ImageClassf.py
@@ -3,25 +3,27 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.layers import Conv2D, Dense, MaxPooling2D, Dropout, Flatten
 from tensorflow.keras.models import Sequential
 
+
 def ImageClassf():
     model = Sequential()
     model.add(Conv2D(120, 60, 3, padding='same', activation='relu',
-                        input_shape=(640, 360, 3)))
+                     input_shape=(640, 360, 3)))
     model.add(MaxPooling2D(pool_size=(65, 25), padding='same'))
     model.add(Dropout(0.5))
-    
+
     model.add(Conv2D(60, 30, 3, padding='same'))
     model.add(MaxPooling2D(pool_size=(60, 25), padding='same'))
     model.add(Dropout(0.5))
-    
+
     model.add(Conv2D(60, 25, 3, padding='same'))
     model.add(MaxPooling2D(pool_size=(60, 25), padding='same'))
     model.add(Dropout(0.5))
-    
+
     model.add(Flatten())
-    model.add(Dense(256, activation = 'relu'))
+    model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.5))
 
     model.add(Dense(2, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='Nadam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='Nadam', metrics=['accuracy'])
     return model
--- Code/main.py
+++ Code/main.py
@@ -253,7 +253,7 @@ def GamePlay():
             else:
                 print("Miss")
 
-                
+
 if __name__ == "__main__":
     physical_devices = tf.config.list_physical_devices("GPU")
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
